ef14e6ef4fe36d92722aa5c8a72f9ad81ffea573
ckaanf <skywlstn777@gmail.com>
2025-07-22 14:52:15 +0900

    fix : product entity 변경 - @GeneratedValue `AUTO`, `UUID` 차이 확인

--- service/product/src/main/java/org/icd4/commerce/domain/product/Product.java
+++ service/product/src/main/java/org/icd4/commerce/domain/product/Product.java
@@ -2,19 +2,19 @@ package org.icd4.commerce.domain.product;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.icd4.commerce.application.command.ProductCreationCommand;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+
+import static java.util.Objects.requireNonNull;
 
 @Getter
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id; // long 자동증가 -> 분산환경에서 중복될 가능성 있어서 UUID 형식으로 변경
     private String sellerId;
     private String name;
@@ -31,23 +31,40 @@ public class Product {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    protected Product() {}
+    protected Product() {
+    }
 
-    public static Product create(ProductCreationCommand command) {
+    public static Product create(ProductCreateRequest request) {
         Product product = new Product();
-        product.id = UUID.randomUUID().toString();
-        product.sellerId = Objects.requireNonNull(command.sellerId(), "Seller ID is required");
-        product.name = Objects.requireNonNull(command.name(), "Product name is required");
-        product.brand = Objects.requireNonNull(command.brand(), "Product brand is required");
-        product.description = Objects.requireNonNull(command.description(), "Product description is required");
-        product.categoryId = Objects.requireNonNull(command.categoryId(), "Category ID is required");
-        product.price = Objects.requireNonNull(command.price(), "Product price is required");
-        product.options = command.options() != null ? new ArrayList<>(command.options()) : new ArrayList<>();
+        product.sellerId = requireNonNull(request.sellerId(), "Seller ID is required");
+        product.name = requireNonNull(request.name(), "Product name is required");
+        product.brand = requireNonNull(request.brand(), "Product brand is required");
+        product.description = requireNonNull(request.description(), "Product description is required");
+        product.categoryId = requireNonNull(request.categoryId(), "Category ID is required");
+        product.price = new ProductMoney(request.priceAmount(), request.priceCurrency());
+        product.options = request.options().stream().map(ProductOption::of).toList();
         product.status = ProductStatus.ON_SALE;
         product.isDeleted = false;
         product.createdAt = LocalDateTime.now(ZoneOffset.UTC);
-        product.updatedAt = LocalDateTime.now(ZoneOffset.UTC);
         return product;
     }
 
+    public void updateInfo(ProductInfoUpdateRequest request) {
+        Product product = new Product();
+        product.name = requireNonNull(request.name(), "Product name is required");
+        product.brand = requireNonNull(request.brand(), "Product brand is required");
+        product.description = requireNonNull(request.description(), "Product description is required");
+        if (request.priceAmount() != null && request.priceCurrency() != null) {
+            product.price = new ProductMoney(request.priceAmount(), request.priceCurrency());
+        }
+//        product.options = request.options().stream().map(ProductOption::of).toList();
+
+    }
+
+    public void changeCategory(String categoryId) {
+    }
+
+    public void changeStatusStopped() {
+
+    }
 }
