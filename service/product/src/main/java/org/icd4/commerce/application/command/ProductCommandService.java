79c03a1d31ee613f2e28840a9f46e0bbb254b173
rue <50974669+yeonchohee@users.noreply.github.com>
2025-07-29 20:40:37 +0900

    [product] feat: 상품 API 추가 및 변경 (#26)
    - 상품 가격 변경, 활성화/비활성화, 삭제 API 추가 및 기존 카테고리 변경
    - 모든 명령 API에서 판매자 ID(X-Seller-Id)를 HTTP 헤더로 일관되게 처리.
    
    Co-authored-by: ych <ych@imbu.co.kr>

--- service/product/src/main/java/org/icd4/commerce/application/command/ProductCommandService.java
+++ service/product/src/main/java/org/icd4/commerce/application/command/ProductCommandService.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.icd4.commerce.adapter.webapi.dto.ProductResponse;
 import org.icd4.commerce.adapter.webapi.dto.event.ProductCreatedEventPayload;
 import org.icd4.commerce.domain.product.model.Product;
-import org.icd4.commerce.domain.product.model.ProductMoney;
-import org.icd4.commerce.domain.product.request.ProductCategoryUpdateRequest;
-import org.icd4.commerce.domain.product.request.ProductCreateRequest;
+import org.icd4.commerce.domain.product.request.*;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
@@ -28,14 +26,14 @@ public class ProductCommandService {
         return ProductResponse.fromDomain(productRegisterService.create(request));
     }
 
-    public ProductResponse changeCategory(String productId, ProductCategoryUpdateRequest request) {
+    public ProductResponse changeCategory(String productId, String sellerId, ProductCategoryUpdateRequest request) {
         return ProductResponse.fromDomain(
-                productModifierService.changeCategory(productId, request.categoryId(), request.sellerId())
+                productModifierService.changeCategory(productId, sellerId, request.categoryId())
         );
     }
     //TODO 초희님 구현
-    public ProductResponse changeProductPrice(String productId, String sellerId, ProductMoney newPrice) {
-        return ProductResponse.fromDomain(productModifierService.changeProductPrice(productId, sellerId, newPrice));
+    public ProductResponse changeProductPrice(String productId, String sellerId, ProductPriceUpdateRequest request) {
+        return ProductResponse.fromDomain(productModifierService.changeProductPrice(productId, sellerId, request.price()));
     }
     //TODO 초희님 구현
     public ProductResponse activate(String productId, String sellerId) {
--- service/product/src/main/java/org/icd4/commerce/domain/product/request/ProductCategoryUpdateRequest.java
+++ service/product/src/main/java/org/icd4/commerce/domain/product/request/ProductCategoryUpdateRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ProductCategoryUpdateRequest(
-        @NotBlank @NotNull String categoryId,
-        @NotBlank @NotNull String sellerId
+        @NotBlank @NotNull String categoryId
 ) {
 }
