4eb7b002754ee233d9a5bbfe49843a95a23461b7
ych <ych@imbu.co.kr>
2025-07-29 17:48:56 +0900

    [product] feat: 상품 API 추가 및 변경
    - 상품 가격 변경, 활성화/비활성화, 삭제 API 추가 및 기존 카테고리 변경
    - 모든 명령 API에서 판매자 ID(X-Seller-Id)를 HTTP 헤더로 일관되게 처리.

--- service/product/src/main/java/org/icd4/commerce/adapter/webapi/ProductApi.java
+++ service/product/src/main/java/org/icd4/commerce/adapter/webapi/ProductApi.java
@@ -7,8 +7,7 @@ import org.icd4.commerce.adapter.webapi.dto.ProductResponse;
 import org.icd4.commerce.adapter.webapi.dto.ProductVariantResponse;
 import org.icd4.commerce.application.command.ProductCommandService;
 import org.icd4.commerce.application.query.ProductQueryService;
-import org.icd4.commerce.domain.product.request.ProductCategoryUpdateRequest;
-import org.icd4.commerce.domain.product.request.ProductCreateRequest;
+import org.icd4.commerce.domain.product.request.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,15 +44,46 @@ public class ProductApi {
         return ResponseEntity.ok(productQueryService.findVariantBySku(skuId));
     }
 
-    @PatchMapping("/{productId}/category")
-    public ResponseEntity<ProductResponse> changeCategory(
-            @PathVariable String productId,
-            @Valid @RequestBody ProductCategoryUpdateRequest request) {
-        return ResponseEntity.ok(productCommandService.changeCategory(productId, request));
-    }
-
     @PostMapping
     public ResponseEntity<ProductResponse> create(@RequestBody ProductCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productCommandService.create(request));
     }
+
+    @PatchMapping("/{productId}/category")
+    public ResponseEntity<ProductResponse> changeCategory(
+            @PathVariable String productId,
+            @RequestHeader("X-Seller-Id") String sellerId,
+            @Valid @RequestBody ProductCategoryUpdateRequest request) {
+        return ResponseEntity.ok(productCommandService.changeCategory(productId, sellerId, request));
+    }
+
+    @PatchMapping("{productId}/price")
+    public ResponseEntity<ProductResponse> changePrice(
+            @PathVariable String productId,
+            @RequestHeader("X-Seller-Id") String sellerId,
+            @Valid @RequestBody ProductPriceUpdateRequest request) {
+        return ResponseEntity.ok(productCommandService.changeProductPrice(productId, sellerId, request));
+    }
+
+    @PatchMapping("{productId}/activate")
+    public ResponseEntity<ProductResponse> activate(
+            @PathVariable String productId,
+            @RequestHeader("X-Seller-Id") String sellerId) {
+        return ResponseEntity.ok(productCommandService.activate(productId, sellerId));
+    }
+
+    @PatchMapping("{productId}/inactivate")
+    public ResponseEntity<ProductResponse> inactivate(
+            @PathVariable String productId,
+            @RequestHeader("X-Seller-Id") String sellerId) {
+        return ResponseEntity.ok(productCommandService.inactivate(productId, sellerId));
+    }
+
+    @DeleteMapping("{productId}")
+    public ResponseEntity<ProductResponse> delete(
+            @PathVariable String productId,
+            @RequestHeader("X-Seller-Id") String sellerId) {
+        return ResponseEntity.ok(productCommandService.deleteProduct(productId, sellerId));
+    }
+
 }
