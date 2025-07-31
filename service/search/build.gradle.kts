8ded98cd9db7b275b9dc475cfedbc9020eac160d
ckaanf <skywlstn777@gmail.com>
2025-07-31 10:13:24 +0900

    fix: 검색엔진 모듈 템플릿 수정

--- service/search/build.gradle.kts
+++ service/search/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
