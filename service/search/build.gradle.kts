a7eeeec6e9e107cfc4e038c1c4b41105f25d5f23
ckaanf <skywlstn777@gmail.com>
2025-08-05 10:56:02 +0900

    feature: - web dependency 추가 - sample 데이터 통합

--- service/search/build.gradle.kts
+++ service/search/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     // add your dependencies here
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
