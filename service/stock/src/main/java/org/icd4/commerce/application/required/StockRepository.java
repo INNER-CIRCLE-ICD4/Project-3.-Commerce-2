ab424c2143f165ac8a4c905391bf4f92c81e69bc
jojunbeom <wns_p@naver.com>
2025-08-12 02:21:02 +0900

    벌크 업데이트를 통한 동시성 이슈 개선

--- service/stock/src/main/java/org/icd4/commerce/application/required/StockRepository.java
+++ service/stock/src/main/java/org/icd4/commerce/application/required/StockRepository.java
@@ -2,10 +2,24 @@ package org.icd4.commerce.application.required;
 
 
 import org.icd4.commerce.domain.Stock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import java.util.Optional;
 
 public interface StockRepository extends Repository<Stock, String> {
     Stock save(Stock stock);
     Optional<Stock> findById(String stockId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Stock s set s.quantity = s.quantity + :quantity, s.updatedAt = local datetime " +
+            "where s.id = :stockId")
+    Integer increaseStock(String stockId, Long quantity);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Stock s set s.quantity = s.quantity - :quantity, s.updatedAt = local datetime " +
+            "where s.id = :stockId and s.quantity >= :quantity")
+    Integer decreaseStock(String stockId, Long quantity);
+
+
 }
